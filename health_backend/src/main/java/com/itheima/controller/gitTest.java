95911fa17543e3c5d2b729d4eafef3fdcd1975c0
SatanCY <1959970441@qq.com>
2024-09-04 16:56:45 +0800

    third commit

--- health_backend/src/main/java/com/itheima/controller/gitTest.java
+++ health_backend/src/main/java/com/itheima/controller/gitTest.java
@@ -7,5 +7,6 @@ package com.itheima.controller;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("11111111");
+        System.out.println("22222222");
     }
 }
