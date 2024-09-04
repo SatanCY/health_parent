07fe240358a180606694b7255963e1ef6b1a194e
SatanCY <1959970441@qq.com>
2024-09-04 17:01:21 +0800

    hot-fix commit

--- health_backend/src/main/java/com/itheima/controller/gitTest.java
+++ health_backend/src/main/java/com/itheima/controller/gitTest.java
@@ -9,5 +9,6 @@ public class gitTest {
         System.out.println("11111111");
         System.out.println("22222222");
         System.out.println("33333333");
+        System.out.println("hot-fix");
     }
 }
