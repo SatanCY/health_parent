7af99b506b27cd51bb5380b77b3b16920dcbbc65
SatanCY <1959970441@qq.com>
2024-09-04 16:56:14 +0800

    second commit

---
+++ health_backend/src/main/java/com/itheima/controller/gitTest.java
@@ -0,0 +1,11 @@
+package com.itheima.controller;
+
+/**
+ * @Author：蔡洋
+ * @Date：2024/9/4 16:55
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("11111111");
+    }
+}
